bf7b0dde843bcb4546bb202713a5dacd5358853d
Mitulgit <166312147+Mitulgit@users.noreply.github.com>
2024-04-07 15:30:19 +0000

    variable start

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountID = 5451421
+var acoountemail = "test01@gmail.com"
+let accountpassword = "Test@4567"
+accountcity = "surat"
+
+let accountstate
+
+acoountemail = "newtest@gmail.com"
+accountpassword = "newtest@567"
+accountcity = "tom"
+
+
+console.log(accountID ,acoountemail, accountpassword ,accountcity)
+
+console.table([accountID ,acoountemail, accountpassword ,accountcity])
